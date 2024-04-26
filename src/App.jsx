dfa2cf20997c07d8a6bd585935eb0d8677978028
mdasif8393 <md.asif.8393@gmail.com>
2024-04-26 12:31:50 +0600

    initial set up code in all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
 import { useState } from "react";
 import "./App.css";
+import AddPost from "./components/AddPost.jsx";
+import EditPost from "./components/EditPost.jsx";
+import Posts from "./components/Posts";
+import initialPosts from "./data/db.js";
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const [posts, setPosts] = useState(initialPosts);
+  const [post, setPost] = useState(null); // post I am editing
+
+  const handleAddPost = (newPost) => {
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+
+    setPosts([
+      ...posts,
+      {
+        id,
+        ...newPost,
+      },
+    ]);
+  };
+
+  const handleDeletePost = (postId) => {
+    if (confirm("Are you sure you want to delete the post?")) {
+      const newPosts = posts.filter((post) => post.id !== postId);
+      setPosts(newPosts);
+    } else {
+      console("You chose not to delete the post!");
+    }
+  };
+
+  const handleEditPost = (updatedPost) => {
+    const updatedPosts = posts.map((post) =>
+      post.id === updatedPost.id ? updatedPost : post
+    );
+
+    setPosts(updatedPosts);
+  };
 
   return (
     <div>
@@ -24,17 +58,8 @@ function App() {
           ) : (
             <EditPost post={post} onEditPost={handleEditPost} />
           )}
-
-          {error && (
-            <>
-              <hr />
-              <div className="error">{error}</div>
-            </>
-          )}
         </div>
       </div>
     </div>
   );
 }
-
-export default App;
